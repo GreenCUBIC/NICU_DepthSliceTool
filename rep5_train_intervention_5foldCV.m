887e824ff2847e050e0ea9fc460370a82b17fe37
Zein Hajj-Ali <zeinhajjali@outlook.com>
2022-09-25 16:01:05 -0400

    HHA version of training script

--- rep5_train_intervention_5foldCV.m
+++ rep5_train_intervention_5foldCV.m
@@ -1,4 +1,4 @@
-filepath = 'C:\Users\Zalamaan\Documents\Repos\depthSliceTool\bagmerge\InterventionDetectionFiles\FirstFrameDepthRGB_origData_PT\';
+filepath = 'C:\Users\Zalamaan\Documents\Repos\depthSliceTool\bagmerge\InterventionDetectionFiles\FirstFrameDepthRGB_origData_PT_HHA\';
 allPts = [1, 2, 5, 6, 8, 9, 10, 11, 13, 14, 15, 16, 17, 21, 22, 23:28, 29:34];
 cvFolds = [0;0;0;0;0;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1];
 cvFolds = [cvFolds, [1;1;1;1;1;0;0;0;0;0;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1]];
@@ -38,20 +38,20 @@ for runNum = 1:5
     
         k = 1;
         for i = trainPts
-            fullFile = fullfile(cd, 'bagmerge', 'InterventionDetectionFiles', 'FirstFrameDepthRGB_origData_PT', strcat('p', num2str(i)));
+            fullFile = fullfile(cd, 'bagmerge', 'InterventionDetectionFiles', 'FirstFrameDepthRGB_origData_PT_HHA', strcat('p', num2str(i)));
             trainFullFiles{k} = fullFile;
             k = k+1;
         end
-        train_imds = imageDatastore(trainFullFiles, 'IncludeSubfolders', true, 'FileExtensions', ['.jpg'], 'LabelSource', 'foldernames');
+        train_imds = imageDatastore(trainFullFiles, 'IncludeSubfolders', true, 'FileExtensions', ['.png'], 'LabelSource', 'foldernames');
         k = 1;
         for i = testPts
-            fullFile = fullfile(cd, 'bagmerge', 'InterventionDetectionFiles', 'FirstFrameDepthRGB_origData_PT', strcat('p', num2str(i)));
+            fullFile = fullfile(cd, 'bagmerge', 'InterventionDetectionFiles', 'FirstFrameDepthRGB_origData_PT_HHA', strcat('p', num2str(i)));
             testFullFiles{k} = fullFile;
             k = k+1;
         end
-        test_imds = imageDatastore(testFullFiles, 'IncludeSubfolders', true, 'FileExtensions', ['.jpg'], 'LabelSource', 'foldernames');
+        test_imds = imageDatastore(testFullFiles, 'IncludeSubfolders', true, 'FileExtensions', ['.png'], 'LabelSource', 'foldernames');
         
-        [net_Context, info_Context,TP,FP,FN,TN,resultTable] = run_contexts_intervention_D(train_imds, test_imds, foldNum);
+        [net_Context, info_Context,TP,FP,FN,TN,resultTable] = run_contexts_intervention_HHA(train_imds, test_imds, foldNum);
     
         TPs(foldNum) = TP;
         TNs(foldNum) = TN;
@@ -59,11 +59,11 @@ for runNum = 1:5
         FNs(foldNum) = FN;
     
         
-        save(strcat('resultTable_YData_PT_run', string(runNum), '_fold', string(foldNum), '.mat'), 'resultTable');
+        save(strcat('resultTable_YData_PT_HHA_run', string(runNum), '_fold', string(foldNum), '.mat'), 'resultTable');
         foldNum = foldNum + 1;
     end
     
-    mat_filename = strcat('run', string(runNum), '__YData_PT_metrics.mat');
+    mat_filename = strcat('run', string(runNum), '__YData_PT_HHA_metrics.mat');
     save(mat_filename, 'TNs', 'TPs', 'FNs', 'FPs');
 end
     
